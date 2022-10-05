3a1e0cb224a677eb22b7273629339f17779d2561
shihabksaleem <shihab.ks56@gmail.com>
2022-10-05 11:30:53 +0530

    first static website

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+// Question1 
+// function none() {
+//     if (targetDiv1.style.display !== "none")
+//         targetDiv1.style.display = "none";
+//     else if (targetDiv2.style.display !== "none")
+//         targetDiv2.style.display = "none";
+//     else if (targetDiv3.style.display !== "none")
+//         targetDiv3.style.display = "none";
+//     else if (targetDiv4.style.display !== "none")
+//         targetDiv4.style.display = "none";
+//     else if (targetDiv5.style.display !== "none")
+//         targetDiv5.style.display = "none";
+//     else if (targetDiv6.style.display !== "none")
+//         targetDiv6.style.display = "none";
+
+// }
+
+const targetDiv1 = document.getElementById('answer1');
+const div1btn = document.getElementById('question1');
+
+div1btn.onclick = function () {
+    // none();
+
+    if (targetDiv1.style.display !== "none")
+        targetDiv1.style.display = "none";
+    else
+
+        targetDiv1.style.display = "block";
+}
+
+const targetDiv2 = document.getElementById('answer2');
+const div2btn = document.getElementById('question2');
+
+div2btn.onclick = function () {
+    // none();
+
+    if (targetDiv2.style.display !== "none")
+        targetDiv2.style.display = "none";
+    else
+
+        targetDiv2.style.display = "block";
+}
+
+const targetDiv3 = document.getElementById('answer3');
+const div3btn = document.getElementById('question3');
+
+div3btn.onclick = function () {
+    // none();
+
+    if (targetDiv3.style.display !== "none")
+        targetDiv3.style.display = "none";
+    else
+        targetDiv3.style.display = "block";
+}
+
+const targetDiv4 = document.getElementById('answer4');
+const div4btn = document.getElementById('question4');
+
+div4btn.onclick = function () {
+    // none();
+
+    if (targetDiv4.style.display !== "none")
+        targetDiv4.style.display = "none";
+    else
+        targetDiv4.style.display = "block";
+}
+
+const targetDiv5 = document.getElementById('answer5');
+const div5btn = document.getElementById('question5');
+
+div5btn.onclick = function () {
+    // none();
+
+    if (targetDiv5.style.display !== "none")
+        targetDiv5.style.display = "none";
+    else
+        targetDiv5.style.display = "block";
+}
+
+const targetDiv6 = document.getElementById('answer6');
+const div6btn = document.getElementById('question6');
+
+div6btn.onclick = function () {
+    // none();
+
+    if (targetDiv6.style.display !== "none")
+        targetDiv6.style.display = "none";
+    else
+        targetDiv6.style.display = "block";
+}
+
+
+
+
